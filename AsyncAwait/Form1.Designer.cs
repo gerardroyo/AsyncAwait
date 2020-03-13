0633bb1a0e195a8e561778d7e229c7111c75ff1a
gerardroyo <gerard.royo@mataro.epiaedu.cat>
2020-03-13 18:11:50 +0100

    frontEnd Ready

--- AsyncAwait/Form1.Designer.cs
+++ AsyncAwait/Form1.Designer.cs
@@ -32,8 +32,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
@@ -44,7 +44,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Sequencial";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -54,7 +54,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "TPL";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -76,21 +76,23 @@
             this.listBox2.TabIndex = 3;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
-            // textBox1
+            // timer1
             // 
-            this.textBox1.Location = new System.Drawing.Point(93, 222);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(296, 20);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.timer1.Location = new System.Drawing.Point(93, 222);
+            this.timer1.Name = "timer1";
+            this.timer1.Size = new System.Drawing.Size(296, 20);
+            this.timer1.TabIndex = 4;
+            this.timer1.Text = "00.000 segundos";
+            this.timer1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // timer2
             // 
-            this.textBox2.Location = new System.Drawing.Point(413, 221);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(294, 20);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.timer2.Location = new System.Drawing.Point(413, 221);
+            this.timer2.Name = "timer2";
+            this.timer2.Size = new System.Drawing.Size(294, 20);
+            this.timer2.TabIndex = 5;
+            this.timer2.Text = "00.000 segundos";
+            this.timer2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button3
             // 
@@ -98,7 +100,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
+            this.button3.Text = "Hora actual";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -108,6 +110,7 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(305, 20);
             this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "Son las ?";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // Form1
@@ -117,8 +120,8 @@
             this.ClientSize = new System.Drawing.Size(800, 342);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.timer2);
+            this.Controls.Add(this.timer1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
@@ -137,8 +140,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox timer1;
+        private System.Windows.Forms.TextBox timer2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox3;
     }
--- AsyncAwait/Form1.cs
+++ AsyncAwait/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace AsyncAwait
 {
     public partial class Form1 : Form
     {
+
+        List<sortida>;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,12 +28,31 @@ namespace AsyncAwait
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 
+            foreach (string d in System.IO.Directory.GetDirectories(@"C:\Users\roshs\Documents\prueba"))
+            {
+                foreach (string s in System.IO.Directory.GetFiles(d))
+                {
+                    sortida.Add(s);
+                    System.Threading.Thread.Sleep(1);
+                }
+            }
+
+            sw.Stop();
+            timer1.Text = clock.Elapsed.TotalSeconds.ToString() + "segundos";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 
+
+
+            sw.Stop();
+            timer2.Text = sw.Elapsed.ToString("hh\\:mm\\:ss\\.fff" + "segundos");
         }
 
         private void button3_Click(object sender, EventArgs e)
